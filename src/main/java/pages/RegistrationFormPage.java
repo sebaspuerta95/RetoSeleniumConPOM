3f04a2869f416dfcce466cca321d401b066ea1cd
Sebastian Puerta <sepuertabe@gmail.com>
2025-02-22 15:32:18 -0500

    Selenium Support dependency was included, methods to fill in the form and click the registration button were included in the RegistrationFormPage

--- src/main/java/pages/RegistrationFormPage.java
+++ src/main/java/pages/RegistrationFormPage.java
@@ -2,12 +2,14 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class RegistrationFormPage {
 
     private WebDriver driver;
     private By enterAccInfo = By.xpath("//h2/b[text()='Enter Account Information']");
     private By mrButton = By.xpath("//input[@value='Mr']");
+    private By mrsButton = By.xpath("//input[@value='Mrs']");
     private By passwordField = By.xpath("//input[@type='password']");
     private By daysDropdown = By.xpath("//select[@id='days']");
     private By monthDropdown = By.xpath("//select[@id='months']");
@@ -16,7 +18,7 @@ public class RegistrationFormPage {
     private By optinBox = By.xpath("//input[@id='optin']");
     private By firstnameField = By.id("first_name");
     private By lastnameField = By.id("last_name");
-    private By companyyField = By.id("company");
+    private By companyField = By.id("company");
     private By addressOneField = By.id("address1");
     private By addressTwoField = By.id("address2");
     private By countryDropdown = By.xpath("//select[@id='country']");
@@ -34,5 +36,44 @@ public class RegistrationFormPage {
         return driver.findElement(enterAccInfo).getText();
     }
 
+    public void fillAccountInfoForm(String gender, String password, String day, String month, String year
+            , String newsletter, String optin, String firstname, String lastname, String company, String addressOne
+            , String addressTwo, String country, String state, String city, String zipCode, String mobileNumber){
+
+        if (gender.equalsIgnoreCase("Male")){
+            driver.findElement(mrButton).click();
+        } else {
+            driver.findElement(mrsButton).click();
+        }
+
+        driver.findElement(passwordField).sendKeys(password);
+        selectFromDropdown(daysDropdown, day);
+        selectFromDropdown(monthDropdown, month);
+        selectFromDropdown(yearDropdown, year);
+        if (newsletter.equalsIgnoreCase("Yes")) {driver.findElement(newsletterBox).click();}
+        if (optin.equalsIgnoreCase("Yes")) {driver.findElement(optinBox).click();}
+        driver.findElement(firstnameField).sendKeys(firstname);
+        driver.findElement(lastnameField).sendKeys(lastname);
+        driver.findElement(companyField).sendKeys(company);
+        driver.findElement(addressOneField).sendKeys(addressOne);
+        driver.findElement(addressTwoField).sendKeys(addressTwo);
+        selectFromDropdown(countryDropdown, country);
+        driver.findElement(stateField).sendKeys(state);
+        driver.findElement(cityField).sendKeys(city);
+        driver.findElement(zipCodeField).sendKeys(zipCode);
+        driver.findElement(numberField).sendKeys(mobileNumber);
+    }
+
+    public void clickCreateAccountButton(){
+        driver.findElement(createAccButton).click();
+    }
+
+    private Select findDropdownElement(By selector){
+        return new Select(driver.findElement(selector));
+    }
+
+    public void selectFromDropdown (By selector, String option) {
+        findDropdownElement(selector).selectByVisibleText(option);
+    }
 
 }
