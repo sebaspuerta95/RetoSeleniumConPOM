7c550d9d2aca428dd067f22f1c4e1a6e859ff5d0
Sebastian Puerta <sepuertabe@gmail.com>
2025-02-22 13:13:47 -0500

    Locators for elements in the SignupLoginPage were included

--- src/main/java/pages/SignupLoginPage.java
+++ src/main/java/pages/SignupLoginPage.java
@@ -1,13 +1,22 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class SignupLoginPage {
 
     private WebDriver driver;
+    private By signUpMessage = By.xpath("//div[@class='signup-form']/h2");
+    private By signUpNameField = By.xpath("//input[@data-qa='signup-name']");
+    private By signUpEmailField = By.xpath("//input[@data-qa='signup-email']");
+    private By signUpButton = By. xpath("//button[@data-qa='signup-button']");
 
     public SignupLoginPage (WebDriver driver){
         this.driver = driver;
+    }
+
+    public String getSignUpMessage (){
+        return driver.findElement(signUpMessage).getText();
     }
 
 }
