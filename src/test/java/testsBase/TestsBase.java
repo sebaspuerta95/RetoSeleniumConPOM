455f237159b566eaebb87802a57c546e3c7330c5
Sebastian Puerta <sepuertabe@gmail.com>
2025-02-22 12:34:48 -0500

    Included TestsBase class as the base for the rest of the classes

---
+++ src/test/java/testsBase/TestsBase.java
@@ -0,0 +1,25 @@
+package testsBase;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestsBase {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://automationexercise.com/test_cases");
+    }
+
+    @AfterEach
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
