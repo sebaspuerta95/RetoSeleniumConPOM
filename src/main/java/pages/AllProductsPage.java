3a4aa74596382682903f7148078ccce6bd319c47
Sebastian Puerta <sepuertabe@gmail.com>
2025-02-22 19:40:26 -0500

    Methods to interact with the page were included to the AllProductsPage class

--- src/main/java/pages/AllProductsPage.java
+++ src/main/java/pages/AllProductsPage.java
@@ -1,15 +1,33 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class AllProductsPage {
 
     private WebDriver driver;
+    @FindBy(xpath = "//div[@class='features_items']")
+    private WebElement allProductsList;
+
+    @FindBy(xpath = "//a[contains(@href, '/product_details')]")
+    private WebElement viewProductButton;
 
     public AllProductsPage (WebDriver driver){
         this.driver = driver;
     }
 
+    public String getPageTitle(){
+        return driver.getTitle();
+    }
+
+    public boolean isAllProductsListVisible(){
+        return allProductsList.isDisplayed();
+    }
+
+    public void clickViewProductButtonOfFirstProductVisible(){
+        viewProductButton.click();
+    }
 
 
 }
