63f227f3cf8b710d753f82933fc28bc2c79f0447
Sebastian Puerta <sepuertabe@gmail.com>
2025-02-22 21:31:31 -0500

    ViewCartPage class was created, no methods yet.

---
+++ src/main/java/pages/ViewCartPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class ViewCartPage {
+
+    private WebDriver driver;
+
+    public ViewCartPage (WebDriver driver){
+        this.driver = driver;
+    }
+
+}
