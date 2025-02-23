231c7c6bab4c84ee79c77bf30e191089588bc4fd
Sebastian Puerta <sepuertabe@gmail.com>
2025-02-22 19:28:09 -0500

    HomePage included handle to AllProductsPage and Page Factory

---
+++ src/main/java/pages/AllProductsPage.java
@@ -0,0 +1,15 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class AllProductsPage {
+
+    private WebDriver driver;
+
+    public AllProductsPage (WebDriver driver){
+        this.driver = driver;
+    }
+
+
+
+}
--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,22 +1,32 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
 
     private WebDriver driver;
-    private By signUpLoginButton = By.xpath("//a[@href='/login']");
+    @FindBy(xpath = "//a[@href='/login']")
+    private WebElement signUpLoginButton;
+
+    @FindBy(xpath = "//a[@href='/products']")
+    private WebElement productsButton;
 
     public HomePage (WebDriver driver){
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public SignupLoginPage clickSignUpLoginButton(){
-        driver.findElement(signUpLoginButton).click();
+        signUpLoginButton.click();
         return new SignupLoginPage(driver);
     }
 
-
+    public AllProductsPage clickProductsButton(){
+        productsButton.click();
+        return new AllProductsPage(driver);
+    }
 
 }
