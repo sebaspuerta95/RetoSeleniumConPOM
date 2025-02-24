5ac4f2ef2953078b65221e99246cda7aba7656fa
Sebastian Puerta <sepuertabe@gmail.com>
2025-02-24 00:47:38 -0500

    CheckoutPage class was updated with methods to interact with desired elements

--- src/main/java/pages/CheckoutPage.java
+++ src/main/java/pages/CheckoutPage.java
@@ -1,13 +1,44 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class CheckoutPage {
 
     private WebDriver driver;
+    @FindBy (xpath = "//h2[text()='Address Details']")
+    private WebElement addressDetailsLabel;
+
+    @FindBy (xpath = "//h2[text()='Review Your Order']")
+    private WebElement reviewOrderLabel;
+
+    @FindBy (xpath = "//textarea[@class='form-control']")
+    private WebElement commentArea;
+
+    @FindBy (xpath = "//a[@href='/payment']")
+    private WebElement placeOrderButton;
 
     public CheckoutPage (WebDriver driver){
         this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public boolean isAddressDetailsLabelVisible(){
+        return addressDetailsLabel.isDisplayed();
+    }
+
+    public boolean isReviewOrderLabelVisible(){
+        return reviewOrderLabel.isDisplayed();
+    }
+
+    public void typeInCommentArea(String comment){
+        commentArea.sendKeys(comment);
+    }
+
+    public void clickPlaceOrderButton(){
+        placeOrderButton.click();
     }
 
 }
