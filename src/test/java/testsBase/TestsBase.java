9d2209110c6f8feeb2be7afbabca47ba1b134f86
Sebastian Puerta <sepuertabe@gmail.com>
2025-02-24 08:45:28 -0500

    TestsBase class was modified to include ChromeOptions to store downloads from the browser

--- src/test/java/testsBase/TestsBase.java
+++ src/test/java/testsBase/TestsBase.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.HomePage;
 
 import java.time.Duration;
+import java.util.HashMap;
 
 public class TestsBase {
 
@@ -18,7 +20,7 @@ public class TestsBase {
     @BeforeEach
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver/chromedriver.exe");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(driverConfiguration());
         driver.manage().window().maximize();
         driver.get("https://automationexercise.com/");
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
@@ -28,6 +30,19 @@ public class TestsBase {
     @AfterEach
     public void tearDown(){
         //driver.quit();
+    }
+
+    private ChromeOptions driverConfiguration(){
+        HashMap<String, Object> chromePrefs = new HashMap<>();
+        chromePrefs.put("download.default_directory", "src/test/resources/invoices");
+        chromePrefs.put("download.prompt_for_download", false);
+        chromePrefs.put("download.directory_upgrade", true);
+        chromePrefs.put("safebrowsing.enabled", true);
+
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", chromePrefs);
+
+        return options;
     }
 
 }
